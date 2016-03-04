dda4da2b65b306a12fd746aaba1974d43ceb55e2
lorello <lorenzo.salvadorini@softecspa.it>
2016-03-04 22:54:21 +0100

    code style fixed using puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,25 +24,25 @@ class supervisor {
   }
 
   file {'/var/log/supervisor/archives':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0664',
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
   }
 
   logrotate::file { 'supervisor':
-    log           => '/var/log/supervisor/supervisord.log',
-    interval      => 'daily',
-    rotation      => '5',
-    options       => [ 'missingok', 'compress', 'notifempty' ],
-    archive       => true,
-    olddir        => '/var/log/supervisor/archives/supervisor',
-    olddir_owner  => 'root',
-    olddir_group  => 'root',
-    olddir_mode   => '664',
-    create        => '664 root root',
-    postrotate    => '/usr/bin/killall -USR2 supervisord',
-    require       => File['/var/log/supervisor/archives'],
+    log          => '/var/log/supervisor/supervisord.log',
+    interval     => 'daily',
+    rotation     => '5',
+    options      => [ 'missingok', 'compress', 'notifempty' ],
+    archive      => true,
+    olddir       => '/var/log/supervisor/archives/supervisor',
+    olddir_owner => 'root',
+    olddir_group => 'root',
+    olddir_mode  => '664',
+    create       => '664 root root',
+    postrotate   => '/usr/bin/killall -USR2 supervisord',
+    require      => File['/var/log/supervisor/archives'],
   }
 
   service { $supervisor::params::system_service:
